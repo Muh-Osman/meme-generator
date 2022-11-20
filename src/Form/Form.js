473ed3a52149eb4c453130be1283f12948075221
Muhammed Osman <91621437+Muh-Osman@users.noreply.github.com>
2022-11-20 11:27:11 +0300

    fix

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -2,25 +2,28 @@ import pic from "../Assets/download.jpg";
 import { useState, useEffect } from "react";
 
 const Form = () => {
-
-
+  // Save input value to show it in image
   const [upValue, setUpValue] = useState("");
   const [downValue, setDownValue] = useState("");
 
-  const upDiv = document.getElementById("updiv");
-  const downdiv = document.getElementById("downdiv");
-
-  useEffect(()=>{},[])
-  
-
+  // Prevent refresh page after submit the form
   const submit = (e) => {
     e.preventDefault();
   };
-
+  
+  // Generate random image
   function generateImage() {
-    // fetch('https://api.imgflip.com/get_memes')
+    const memesArray = memsData.data.memes
+    const randomNum = Math.floor(Math.random() * memesArray.length)
+    const url = memesArray[randomNum].url
   }
+  
+  useEffect(() => {
+    // fetch('https://api.imgflip.com/get_memes')
+    .then((response) => response.json())
+    .then((data) => console.log(data));
 
+  }, []);
 
 
   return (
@@ -47,9 +50,18 @@ const Form = () => {
 
       <div className="img-box">
         <img src={pic} />
-        <div className="up-text" id="updiv" dangerouslySetInnerHTML={{ __html: upValue }}></div>
-        <div className="down-text" id="downdiv" dangerouslySetInnerHTML={{ __html: downValue }}></div>
+        <div
+          className="up-text"
+          dangerouslySetInnerHTML={{ __html: upValue }}
+        ></div>
+        <div
+          className="down-text"
+          dangerouslySetInnerHTML={{ __html: downValue }}
+        ></div>
       </div>
+      <p>
+        API used <a href="https://imgflip.com/api">imgflip</a>
+      </p>
     </main>
   );
 };
